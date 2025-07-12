55313a1ed19b5a09a3b93c90b4974ed8dcf748bb
Douglas Sabwa Indumwa <dglsdeveloper@gmail.com>
2025-07-12 14:35:18 +0300

    Update: Remove autoload dot env

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+// Initiate db connection
+try {
+    $dsn = "mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']};charset=utf8mb4";
+    $pdo = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS'], [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+} catch (PDOException $e) {
+    exit("END Database connection failed.");
+}
+?>
